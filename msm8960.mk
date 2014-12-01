143b7f9ce068177461aae7c4489b70ef293c80ff
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2014-12-01 13:00:59 +0100

    include ffmpeg codec
    PS2: include ffmpeg after google's codecs
    
    Change-Id: I48bce8dc05d7d5ddefa27fc0bd9eaa711e03c3c1

--- msm8960.mk
+++ msm8960.mk
@@ -18,6 +18,7 @@ $(call inherit-product, device/sony/qcom-common/qcom-common.mk)
 COMMON_PATH := device/sony/msm8960-common
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
